8b6202d791a1d060f7c2883c686b954b6075d46d
Мотричкин Кирилл Владиславович <KVMotrichkin@sberbank.ru>
2022-01-19 13:01:37 +0300

    Colors fix

--- src/main/java/com/github/superkiria/chess/color/ColorHelper.java
+++ src/main/java/com/github/superkiria/chess/color/ColorHelper.java
@@ -11,41 +11,41 @@ public class ColorHelper {
     private static final int W_MAX = 250;
 
     private static final int[][] blackColors = {
-            {MIN, MIN, MAX},
-            {MIN, MAX, MIN},
+            {MIN, 120, MAX},
+            {103, 170, 103},
             {MAX, MIN, MIN},
 
-            {MIN, MAX, MAX},
-            {MAX, MAX, MIN},
+            {103, 192, 192},
+            {192, 192, 103},
             {MAX, MIN, MAX},
 
-            {MAX, MID, MIN},
-            {MIN, MAX, MID},
-            {MID, MIN, MAX},
+            {MAX - 5, MID - 5, MIN - 5},
+            {100, 180, 140},
+            {165, 128, 210},
 
             {MAX, MIN, MID},
-            {MID, MAX, MIN},
-            {MIN, MID, MAX},
+            {140, 180, 100},
+            {103, 162, 215},
 
     };
 
     private static final int[][] whiteColors = {
 
-            {W_MIN, W_MIN, W_MAX},
-            {W_MIN, W_MAX, W_MIN},
-            {W_MAX, W_MIN, W_MIN},
+            {224, W_MIN, 242},
+            {W_MIN, 240, 224},
+            {240, W_MIN, 224},
 
-            {W_MIN, W_MAX, W_MAX},
-            {W_MAX, W_MAX, W_MIN},
-            {W_MAX, W_MIN, W_MAX},
+            {W_MIN, W_MAX, 242},
+            {242, 242, W_MIN},
+            {242, 224, 242},
 
             {W_MAX, W_MID, W_MIN},
-            {W_MIN, W_MAX, W_MID},
-            {W_MID, W_MIN, W_MAX},
+            {W_MIN, W_MAX - 10, W_MID},
+            {W_MID, W_MIN, W_MAX - 10},
 
-            {W_MAX, W_MIN, W_MID},
-            {W_MID, W_MAX, W_MIN},
-            {W_MIN, W_MID, W_MAX},
+            {W_MAX - 10, W_MIN, W_MID},
+            {W_MID, W_MAX - 10, W_MIN},
+            {224, 239, 242},
 
     };
 
