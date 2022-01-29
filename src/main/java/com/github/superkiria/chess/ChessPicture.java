852c1a8c6d93701e9a07aa7c15220319cb498d4d
Kirill Mot <kirill.motrichkin@ya.ru>
2022-01-29 22:08:30 +0300

    Refactoring III

--- src/main/java/com/github/superkiria/chess/ChessPicture.java
+++ src/main/java/com/github/superkiria/chess/ChessPicture.java
@@ -1,25 +1,59 @@
 package com.github.superkiria.chess;
 
+import com.github.superkiria.chess.color.BoardColor;
 import com.github.superkiria.chess.svg.SvgFileNames;
 import com.github.superkiria.chess.svg.SvgPiece;
 import org.w3c.dom.svg.SVGDocument;
 
-import java.io.IOException;
+import java.io.ByteArrayOutputStream;
 import java.util.EnumMap;
-import java.util.Objects;
-
-import static com.github.superkiria.chess.svg.SvgUtils.createSVGDocumentFromFile;
 
 public class ChessPicture {
 
     private final SVGDocument empty;
-    private final EnumMap<SvgFileNames, SvgPiece> filesForPieces = new EnumMap<>(SvgFileNames.class);
+    private final EnumMap<SvgFileNames, SvgPiece> filesForPieces;
+    private final BoardColor color;
 
-    public ChessPicture() throws IOException {
-        empty = createSVGDocumentFromFile(Objects.requireNonNull(getClass().getClassLoader().getResource("svg/empty.svg")).toString());
-        for (SvgFileNames entry : SvgFileNames.values()) {
-            filesForPieces.put(entry, new SvgPiece(Objects.requireNonNull(getClass().getClassLoader().getResource(entry.getFileName())).toString()));
-        }
+    protected ChessPicture(SVGDocument empty, EnumMap<SvgFileNames, SvgPiece> filesForPieces, BoardColor color) {
+        this.empty = empty;
+        this.filesForPieces = filesForPieces;
+        this.color = color;
+    }
+
+    public void setPositionFromPgn(String pgn) {
+        setPositionFromPgn(pgn, false);
+    }
+
+    public void setPositionFromPgn(String pgn, boolean highlightLastMove) {
+
+    }
+
+    public void setPositionFromFen(String fen) {
+
+    }
+
+    public void setPositionFromFen(String fen, String moveToHighlight) {
+
+    }
+
+    public void addPiece(String piece) {
+
+    }
+
+    public void highlightField(String move) {
+
+    }
+
+    public SVGDocument getDocument() {
+        return null;
+    }
+
+    public String getXmlDocument() {
+        return null;
+    }
+
+    public ByteArrayOutputStream generatePng() {
+        return null;
     }
 
 }
