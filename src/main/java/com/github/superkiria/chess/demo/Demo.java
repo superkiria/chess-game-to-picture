ac1faab1dbcc3ed288013d0716738bd5bb05abf6
Kirill Mot <kirill.motrichkin@ya.ru>
2022-06-19 18:38:58 +0300

    Color fix

--- src/main/java/com/github/superkiria/chess/demo/Demo.java
+++ src/main/java/com/github/superkiria/chess/demo/Demo.java
@@ -1,5 +1,6 @@
 package com.github.superkiria.chess.demo;
 
+import com.github.superkiria.chess.color.BoardColor;
 import com.github.superkiria.chess.svg.SvgBoardBuilder;
 
 import static com.github.superkiria.chess.svg.SvgUtils.documentToXmlString;
@@ -8,7 +9,7 @@ import static com.github.superkiria.chess.svg.SvgUtils.saveDocumentToPng;
 public class Demo {
 
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < BoardColor.values().length; i++) {
             SvgBoardBuilder builder = new SvgBoardBuilder();
             builder.setLastMoveInNotation("b8d7");
             builder.setFen("r1bqkb1r/pp1npppp/3p1n2/8/3P4/5N1P/PPP2PP1/RNBQKB1R w KQkq - 1 7");
